c5c9e9188398796563f14a20636533e73210e49e
Ariful islam <129893986+BD-Arfat@users.noreply.github.com>
2023-07-28 19:19:29 +0600

    Update AddProducts.js

--- src/Components/AddProducts/AddProducts.js
+++ src/Components/AddProducts/AddProducts.js
@@ -3,12 +3,14 @@ import Form from './Form';
 
 const AddProducts = () => {
     return (
+        // start Add Products
         <div className='mt-10'>
             <h1 className='text-4xl font-bold text-center'>You add your product</h1>
             <div className='mt-16 mb-20'>
                 <Form />
             </div>
         </div>
+    // end Add Products
     );
 };
 
